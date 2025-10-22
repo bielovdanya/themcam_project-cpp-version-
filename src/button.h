1cb1e7213eaf77fb8f34e597c719f7030940cdb0
Danylo Bielov <166961384+bielovdanya@users.noreply.github.com>
2025-10-22 13:23:47 +0300

    Delete src directory

--- src/button.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "main.h"
-
-void initButton();
-bool buttonPressed();
--- src/display.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino_GFX_Library.h>
-#include "main.h"
-
-extern Arduino_GFX *gfx;
-void initDisplay();
-void drawThermal(const float *buf, float tMin, float tMax);
-void drawPanel(float tMin, float tMax, bool paused);
